71800bb6e193d7ed1a06a9ac5e673952ae0b9b6b
Kanishk Mishra <Kmishra7@uwo.ca>
2024-11-04 18:42:20 -0500

    Update js error that create backlog task

--- backlog.js
+++ backlog.js
@@ -1,4 +1,4 @@
-// Create
+// Create backlog tasks
 function createBacklogTask(htmlStr) {
     var frag = document.createDocumentFragment(),
         temp = document.createElement('div');
@@ -13,6 +13,6 @@ var taskTitle = "BACKLOG TASK";
 var description = "We need to do stuff";
 var storyPoints = 1;
 
-const fragment = create(`<div class="task"><h2>${taskTitle}</h2><p>Description: ${description}</p><q1>Story Points: ${storyPoints}</q1></div>`);
+const fragment = createBacklogTask(`<div class="task"><h2>${taskTitle}</h2><p>Description: ${description}</p><q1>Story Points: ${storyPoints}</q1></div>`);
 
-document.body.insertBefore(fragment, document.getElementsByClassName('List')[0]);
+document.body.insertBefore(fragment, document.getElementsByClassName("List")[0]);
