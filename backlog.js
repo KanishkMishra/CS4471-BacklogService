57346540d5f5df1935b9b7d429d50d56f8d6bf82
Kanishk Mishra <Kmishra7@uwo.ca>
2024-11-04 16:33:54 -0500

    Create initial files with html, css, and js

---
+++ backlog.js
@@ -0,0 +1,17 @@
+// Create
+function createBacklogTask(htmlStr) {
+    var frag = document.createDocumentFragment(),
+        temp = document.createElement('div');
+    temp.innerHTML = htmlStr;
+    while (temp.firstChild) {
+        frag.appendChild(temp.firstChild);
+    }
+    return frag;
+}
+
+var taskTitle = "BACKLOG TASK";
+var description = "We need to do stuff";
+var storyPoints = 1;
+
+var fragment = create(`<div class="task"><h2>${taskTitle}</h2><p>Description: ${description}</p><q1>Story Points: ${storyPoints}</q1></div>`);
+document.body.insertBefore(fragment, document.getElementsByClassName('List')[0]);
