06f2827f8e1d963de86d2906de3dac2b84fdcfd7
Kanishk Mishra <Kmishra7@uwo.ca>
2024-11-10 13:18:59 -0500

    Display daily data section by each day

--- service.js
+++ service.js
@@ -11,14 +11,14 @@ function createBacklogTask(htmlStr) {
 
 // access api
 const apiKey = 'I24DBXT85LCQ72AP';
-const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=1min&apikey=' + apiKey;
+const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=' + apiKey;
 
 // Retrieve specific data
 function getAlphaVantagedata() {
 
 		const symbol = inpSymbol.value;
 
-		const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=' + symbol + '&interval=1min&apikey=' + apiKey;
+		const url = 'https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=' + symbol + '&apikey=' + apiKey;
 
 		requestFile( url );
 
@@ -39,9 +39,46 @@ function requestFile( url ) {
 			  let response, json, lines;
 
 			  response = xhr.target.response;
-			  divContents.innerText = response;
+			  //divContents.innerText = response;
 
 			  json = JSON.parse( response );
+        json = JSON.parse(`{
+    "Meta Data": {
+        "1. Information": "Daily Prices (open, high, low, close) and Volumes",
+        "2. Symbol": "IBM",
+        "3. Last Refreshed": "2024-11-08",
+        "4. Output Size": "Compact",
+        "5. Time Zone": "US/Eastern"
+    },
+    "Time Series (Daily)": {
+        "2024-11-08": {
+            "1. open": "214.1600",
+            "2. high": "216.7000",
+            "3. low": "212.7809",
+            "4. close": "213.7200",
+            "5. volume": "3201038"
+        }}}`);
+      
+			  for (const category in json)
+			  {
+          if (category == "Meta Data")
+            continue;
+          
+					for (const item in json[category])
+					{
+              for (const key in item)
+              {
+
+              }
+            const fragment = createBacklogTask(`<div class="task"><h2>${category}</h2><p>Date: ${item}</p><p>Values: ${JSON.stringify(json[category][item])}</p></div>`);
+						document.body.insertBefore(fragment,        document.getElementsByClassName("List")[0]);
+            //divContents.innerText = item;
+
+					}
+			  }
+			  /*Object.entries(json).forEach(([key, value]) => {
+				console.log(`${key}: ${value}`);
+			  });*/
 
         console.log( 'json', json );
 	}
@@ -49,10 +86,10 @@ function requestFile( url ) {
 }
 
 // list data
-var title = "STOCK ITEM";
+/*var title = "STOCK ITEM";
 var description = "We need some details";
 var value = 1;
 
 const fragment = createBacklogTask(`<div class="task"><h2>${title}</h2><p>Description: ${description}</p><q1>Value: ${value}</q1></div>`);
 
-document.body.insertBefore(fragment, document.getElementsByClassName("List")[0]);
+document.body.insertBefore(fragment, document.getElementsByClassName("List")[0]);*/
